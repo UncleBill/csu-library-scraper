0a089a53936d73b21c8341319f4a982112a12e28
UncleBill <billbill290@gmail.com>
2013-01-30 00:22:58 +0800

    add file

---
+++ bookInfo.sql
@@ -0,0 +1,14 @@
+create table bookInfo (
+    id integer primary key,
+    call_num    text,
+    bar_code    text,
+    login_num   text,
+    store_loca  text,
+    lend_date   text,
+    return_date text,
+    tran_type   text,
+    order_handle    text,
+    volumn_info text,
+    commit_time date,
+    alt_time    date
+);
