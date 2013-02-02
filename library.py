c6dba6fe41637c86b52c352a68cbe66d26779721
UncleBill <billbill290@gmail.com>
2013-02-02 17:36:07 +0800

    class library's first running success

--- library.py
+++ library.py
@@ -4,12 +4,16 @@
     database file:  library.db
 """
 import sqlite3
-import getSoup
+import time
+
+from getSoup import getSoup
 import data
 
 __INFO__ = 0
 __STORE__ = 1
-__STEP__ = 1000
+__STEP__ = 10
+__CHUNK__ = 5
+
 class library:
     """docstring for library"""
     def __init__(self, database = 'library.db'):
@@ -38,7 +42,7 @@ class library:
         _script = 'select max(recno) from books'
         _max = self.db_con.execute( _script ).fetchone()[0]
         if not _max:
-            _max = 0
+            _max = 1
         return _max
 
     def get_lowest(self,lst):
@@ -56,10 +60,12 @@ class library:
         _script = 'select recno from books order by recno'
         recno_in_db = self.db_con.execute( _script ).fetchall()
         recno_list = [ i[0] for i in recno_in_db ]
+        if len( recno_list ) == 0:
+            return [0]
 
         ignore = self.get_lowest( recno_list )
         _max = self.get_max_rec()
-        check_region = range( ignore, max, 1 )
+        check_region = range( ignore, _max, 1 )
         failList = list( set(check_region) - set(recno_list) )
         print 'failed list is built, contains failed:',len( failList )
         return failList
@@ -74,81 +80,105 @@ class library:
         print '>>commit!(',l,'books )'
         print time.ctime()
         print 'total commit',self.total['commit']
-        print 'total failed',totalFaileds
+        print 'total failed',self.total['fail']
         print 'since last time', time.time() - self.last_time,'s'
 
         self.last_time = time.time()
 
     def commit2db(self):
         _con = self.db_con
-        kl = keyslit
-
         # insert basic info
         for item in self.booklist[__INFO__]:
-            keys_tuple = tuple(  item[i] for i in kl )
-            conn.execute( insertScript ,keys_tuple )
-            _infos_list = item[u'info']
-            for _i in _infos_list:
-                print _i[u'RECNO']
-                info_value_tuple = tuple( _i[i] for i in self.info_entries )
-                _con.execute( self.info_script, info_value_tuple )
-
+            if not item:
+                continue
+            keys_tuple = tuple(  item[i] for i in self.info_entries )
+            _con.execute( self.info_script ,keys_tuple )
         # insert store info
         for line in self.booklist[__STORE__]:
             for item in line:
-                store_value_tuple = tuple( _i[i] for i in self.store_entries )
+                if not item:
+                    continue
+                l = [item[i] for i in self.store_entries]
+                store_value_tuple = tuple( l )
                 _con.execute( self.store_script, store_value_tuple )
 
         _con.commit()
         self.booklist = [ [], [] ]      # rempty booklist
 
-    def basic_parser(soup):
+    def nobook(self,a,b):
+        print a,b
+    def basic_parser(self, soupJar):
+        soup = soupJar[0]
+        num = soupJar[1]
         info_jar = soup.findAll('div','info')
-        if len( info ) < 2: # book doesn't exist
-            self.nobook( num, 'There is not this book.Set to None' )
-            kl = keyslit
+        info = {}
+        if len( info_jar ) < 2: # book doesn't exist
+            #self.nobook( num, 'There is not this book.Set to None' )
+            kl = self.info_entries
             for k in kl:
                 info[k] = u''
             info[u'书名'] = u'No existing!'
-            info[u'RENO'] = num
+            info[u'RECNO'] = num
             return info
 
-    def storeparser(soup):
+        info[u'书名'] = info_jar[1].h1.a.text
+        lst = soup.fetch('ul','list')       # get details of book's info
+        lst = lst[0]
+        details = lst.findAll('li')
+        for item in details:
+            t = item.span.next
+            name =  str( t ).decode('utf-8')
+            t = t.next
+            if name == u'作者':t = t.next
+            value = str( t )
+            info[name] = value.decode('utf-8')
+        info[u'RECNO'] = num
+        return info
+
+    def storeparser(self, soupJar):
+        soup = soupJar[0]
+        num = soupJar[1]
         tbody = soup.fetch( 'tbody' )
         store = {}
         store_list = []
-        if len( tbody ) > 1:
+        if len( tbody ) > 1 or len(tbody) < 1:
             print '!alert: not just a table'
             return None
         trs = tbody[0].fetch('tr')
         if len( trs ) == 0:     # when no store info.
             for i in range(10):
                 store[ self.store_entries[i] ] = 'Empty Info'
+            store['RECNO'] = num
             store_list.append( store )
             return store_list
         for tr in trs:          # save in a store info. list
             tds = tr.fetch('td')
             for i in range(10):
                 store[ self.store_entries[i] ] = tds[i].next
+            store['RECNO'] = num
             store_list.append( store)
         return store_list
 
     def start(self):
-        for i in region:
-            _soup = getSoup(i).soup
-            if not _soup:
+        for i in self.region:
+            _soupJar = getSoup(i).soupJar
+            if not _soupJar:
                 # handle error page
                 self.total_fail += 1
                 continue
-            info = basic_parser(_soup)
-            store = storeparser(_soup)
+            info = self.basic_parser(_soupJar)
+            store = self.storeparser(_soupJar)
             self.booklist[__INFO__].append(info)
             self.booklist[__STORE__].append(store)
-            if i % self.CHUNK == 0:
+            if i % __CHUNK__ == 0:
                 self.commit()
 
 def main():
+    beg = time.time()
+    print time.ctime(),beg
     library().start()
+    print time.ctime()
+    print 'use time:',time.time() - beg
 
 if __name__ == '__main__':
     main()
