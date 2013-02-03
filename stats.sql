8bc9e8068362e46b3c1108fb107fe3f4eaa5da40
UncleBill <billbill290@gmail.com>
2013-02-03 21:39:47 +0800

    add ? ;

--- stats.sql
+++ stats.sql
@@ -1,8 +1,9 @@
+.nullvalue 'NULL'
 select 'how many books?';
 select count(*) from books;
 
-select 'bad books';
-select count(*) from books where bookname = 'No existing!'
+select 'bad books?';
+select count(*) from books where bookname = 'No existing!';
 
-select 'store stats';
+select 'store stats?';
 select count(*),store_state from bookInfos group by store_state;
