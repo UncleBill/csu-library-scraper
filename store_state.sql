0192cdb47d0a541cb49337a612ebf294175aeed6
UncleBill <billbill290@gmail.com>
2013-02-03 16:56:55 +0800

    combine sql statments; and remove store_state.sql

--- store_state.sql
+++ store_state.sql
@@ -1,2 +1,8 @@
+select 'how many books?';
 select count(*) from books;
+
+select 'bad books';
+select count(*) from books where bookname = 'No existing!'
+
+select 'store stats';
 select count(*),store_state from bookInfos group by store_state;
