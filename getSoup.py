8b3a0fbeb998ec736a232848526895eef372ded6
UncleBill <billbill290@gmail.com>
2013-02-10 00:03:22 +0800

    add getSoup2 for newLibrary

--- getSoup.py
+++ getSoup.py
@@ -3,14 +3,14 @@
 import urllib2
 import BeautifulSoup as bs
 
-__TRY__ = 3
+__TRY__ = 4
 
 class getSoup():
     """docstring for getSoup"""
     def __init__(self, num):
-        base = 'http://opac.its.csu.edu.cn/NTRdrBookRetrInfo.aspx?BookRecno='
+        self.base = 'http://opac.its.csu.edu.cn/NTRdrBookRetrInfo.aspx?BookRecno='
         self.num = num
-        self.url = base + str(num)
+        self.url = self.base + str(num)
         self.soupJar = self.getSoup()
 
     def curl(self):
@@ -18,13 +18,16 @@ class getSoup():
         page = False
         while attempts < __TRY__:
             try:
-                page = urllib2.urlopen(self.url)
+                page = urllib2.urlopen(self.url,timeout=100)
                 break
             except:
                 attempts += 1
-        #if attempts == __TRY__ - 1:
-            #print 'overtry'
-        print attempts,
+        if attempts == 0:
+            print '-',
+        elif attempts == __TRY__:
+            print '#',
+        else:
+            print attempts,
         return page
 
     def getSoup(self):
@@ -34,6 +37,42 @@ class getSoup():
         try:
             soup = bs.BeautifulSoup( page )
             return [soup, self.num]
-        except TypeError:
+        except:
+            print 'type error',self.num
+            return None
+
+class getSoup2(getSoup):
+    """getSoup for newLibrary"""
+    def __init__(self,num):
+        self.num = num
+        self.base = 'http://opac.its.csu.edu.cn/NTRdrBookRetr.aspx?strType=text&strKeyValue=*&strpageNum=100&strSort=desc&page='
+        self.url = self.base + str(num)
+        self.soupJar = self.getSoup()
+
+    def curl(self):
+        attempts = 0
+        page = False
+        while attempts < __TRY__:
+            try:
+                page = urllib2.urlopen(self.url,timeout=10)
+                break
+            except:
+                attempts += 1
+        if attempts == 0:
+            print '-',
+        elif attempts == __TRY__:
+            print '#',
+        else:
+            print attempts,
+        return page
+
+    def getSoup(self):
+        page = self.curl()
+        if not page:
+            return None
+        try:
+            soup = bs.BeautifulSoup( page )
+            return [soup,self.num]
+        except:
             print 'type error'
             return None
