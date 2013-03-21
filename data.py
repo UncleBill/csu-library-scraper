433f411a30a547515b8edf8a6d2af126980409f1
UncleBill <billbill290@gmail.com>
2013-03-21 20:13:52 +0800

    correct data struction

--- data.py
+++ data.py
@@ -25,3 +25,29 @@ info_script = u"""insert into books (
     ,recno
     ,which_page
     ,commit_time) values ( ?,?,?,?,?,?,?,?,?,?,?,datetime('now') )"""
+# ########################################
+store_entries = [
+    u'索书号'
+    ,u'条形码'
+    ,u'登录号'
+    ,u'馆藏地点'
+    ,u'管藏状态'
+    ,u'借出日期'
+    ,u'还回日期'
+    ,u'流通类型'
+    ,u'预约处理'
+    ,u'卷册说明'
+    ,u'RECNO']
+store_script = """insert into bookInfos (
+    ,commit_time
+    ,call_num
+    ,bar_code
+    ,login_num
+    ,store_loca
+    ,store_state
+    ,lend_date
+    ,return_date
+    ,tran_type
+    ,order_handle
+    ,volumn_info
+    ,recno) values ( datetime('now'), ?,?,?,?,?,?,?,?,?,?,? )"""
